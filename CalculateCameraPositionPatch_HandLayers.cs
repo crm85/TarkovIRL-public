73d548ee18cf116549553063958d0b067cb4e5c4
colin.r.muller@gmail.com <colin.r.muller@gmail.com>
2024-07-31 19:53:40 +0200

    Refactored entire hands layer
    prob broke a lot of stuff

--- CalculateCameraPositionPatch_HandLayers.cs
+++ CalculateCameraPositionPatch_HandLayers.cs
@@ -17,20 +17,6 @@ namespace TarkovIRL
         private static FieldInfo playerField;
         private static FieldInfo fcField;
 
-        private static float _rotationAvgSetX = 0;
-        private static float _rotationAvgSetY = 0;
-        private static float _rotationAvgX = 0;
-        private static float _rotationAvgY = 0;
-
-        private static float _rotationLerpPosX = 0;
-        private static float _rotationLerpPosY = 0;
-
-        private static float _rotationLerpRotX = 0;
-
-        private static Vector2 _playerRotationLastFrame = Vector2.zero;
-
-
-
         protected override MethodBase GetTargetMethod()
         {
             playerField = AccessTools.Field(typeof(Player.FirearmController), "_player");
@@ -53,52 +39,26 @@ namespace TarkovIRL
             Player player = (Player)playerField.GetValue(firearmController);
             if ((UnityEngine.Object)(object)player != (UnityEngine.Object)null && player.IsYourPlayer)
             {
-                Vector3 addedBreathPosition = HandBreathController.GetModifiedHandPosForBreath(player);
-                Vector3 addedPosePosition = HandMovController.GetModifiedHandPosWithPose(player);
-                Vector3 addedChangePosePos = HandMovController.GetModifiedHandPosWithPoseChange(player);
-                Vector3 addedArmJitterPos = HandShakeController.GetHandsShakePosition(player);
+                Vector3 breathOffset = HandBreathController.GetModifiedHandPosForBreath(player);
+                Vector3 armStamOffset = HandShakeController.GetHandsShakePosition(player);
+                Vector3 poseOffset = HandPoseController.GetModifiedHandPosWithPose(player);
+                Vector3 changePoseOffset = HandPoseController.GetModifiedHandPosWithPoseChange(player);
+
+                Vector3 movementZOffsets = HandMovController.GetModifiedHandPosZMovement(player);
+                Vector3 unstockedOffset = HandMovController.GetModifiedHandPosForUnstockedMovement(player);
+
 
                 bool isBreathPos = PrimeMover.IsBreathingEffect.Value;
                 bool isPosePos = PrimeMover.IsPoseEffect.Value;
                 bool isPoseChangePos = PrimeMover.IsPoseChangeEffect.Value;
                 bool isArmShake = PrimeMover.IsArmShakeEffect.Value;
 
-                if (isBreathPos) __instance.HandsContainer.WeaponRoot.localPosition += addedBreathPosition;
-                if (isPosePos) __instance.HandsContainer.WeaponRoot.localPosition += addedPosePosition;
-                if (isPoseChangePos) __instance.HandsContainer.WeaponRoot.localPosition += addedChangePosePos;
-                if (isArmShake) __instance.HandsContainer.WeaponRoot.localPosition += addedArmJitterPos;
-
-                Vector3 addedZPos = HandMovController.GetModifiedHandPosForRotSpeed(player);
-                Vector3 addedStockedMovPos = HandMovController.GetModifiedHandPosForUnstockedMovement();
-
-                __instance.HandsContainer.WeaponRoot.localPosition += addedZPos;
-                __instance.HandsContainer.WeaponRoot.localPosition += addedStockedMovPos;
-
-
-                // testing
-                Vector2 rotationalMotionThisFrame = _playerRotationLastFrame - player.Rotation;
-                _playerRotationLastFrame = player.Rotation;
-
-                _rotationAvgSetX += rotationalMotionThisFrame.x;
-                _rotationAvgSetY += rotationalMotionThisFrame.y;
-                _rotationAvgSetX -= _rotationAvgX;
-                _rotationAvgSetY -= _rotationAvgY;
-                _rotationAvgSetX = Mathf.Clamp(_rotationAvgSetX, -PrimeMover.DevTestFloat1.Value, PrimeMover.DevTestFloat1.Value);
-                _rotationAvgSetY = Mathf.Clamp(_rotationAvgSetY, -PrimeMover.DevTestFloat1.Value, PrimeMover.DevTestFloat1.Value);
-                _rotationAvgX = _rotationAvgSetX * player.DeltaTime;
-                _rotationAvgY = _rotationAvgSetY * player.DeltaTime;
-
-
-                float lerpRate = player.DeltaTime * 2f;
-                _rotationLerpPosX = Mathf.Lerp(_rotationLerpPosX, _rotationAvgX, lerpRate * PrimeMover.DevTestFloat3.Value);
-                //_rotLerpY = Mathf.Lerp(_rotLerpY, _rotAvgY, lerpRate);
-                __instance.HandsContainer.WeaponRoot.localPosition = __instance.HandsContainer.WeaponRoot.localPosition + new Vector3(_rotationLerpPosX * PrimeMover.DevTestFloat4.Value, 0, 0);
-                // this works ^ 
-
-
-                _rotationLerpRotX = Mathf.Lerp(_rotationLerpRotX, _rotationAvgX, lerpRate * PrimeMover.DevTestFloat.Value);
-                //__instance.HandsContainer.Weapon.localRotation = __instance.HandsContainer.Weapon.localRotation * Quaternion.Euler(0, PrimeMover.DevTestFloat1.Value, 0);
-                __instance.HandsContainer.Weapon.localRotation = __instance.HandsContainer.Weapon.localRotation * Quaternion.Euler(0, 0, -_rotationLerpRotX * PrimeMover.DevTestFloat2.Value);
+                if (isBreathPos) __instance.HandsContainer.WeaponRoot.localPosition += breathOffset;
+                if (isPosePos) __instance.HandsContainer.WeaponRoot.localPosition += poseOffset;
+                if (isPoseChangePos) __instance.HandsContainer.WeaponRoot.localPosition += changePoseOffset;
+                if (isArmShake) __instance.HandsContainer.WeaponRoot.localPosition += armStamOffset;
+                __instance.HandsContainer.WeaponRoot.localPosition += movementZOffsets;
+                __instance.HandsContainer.WeaponRoot.localPosition += unstockedOffset;
             }
         }
     }
--- HandMovController.cs
+++ HandMovController.cs
@@ -12,128 +12,40 @@ namespace TarkovIRL
 {
     public static class HandMovController
     {
-        static float _lerpRate = 4f;
-        static float _currentLerpRate = 0;
+        // readonlys
+        static readonly float _RotPullInDeltaThresh = 0.0002f;
+        static readonly float _RotPullInValue = 0.05f;
+        static readonly float _LerpRate = 4f;
+        static readonly float _PullInGateTime = 0.1f;
+        static readonly float _StockMovementAddedPosValue = 0.01f;
 
-        static float poseVerticalPoseOffsetModifier = .03f;
-        static float poseProjectedPoseOffsetModifier = -0.05f;
-
-        static float _poseVerticalPoseOffsetTarget = 0;
-        static float _poseProjectedPoseOffsetTarget = 0;
-
-        static float _poseVerticalPoseOffsetLerp = 0;
-        static float _poseProjectedPoseOffsetLerp = 0;
-
-        static float _poseLevelLastFrame = 0;
-
-        static Vector3 poseShiftVector = Vector3.zero;
-        static bool _isChangingPose = false;
-        static float _changingPoseCycle = 0;
-        static float _vertDiff = 0;
-        static float _changePoseModifier = 0.01f;
-
+        // vars
         static float _rotPullInTarget = 0;
         static float _rotPullInLerp = 0;
-        static readonly float RotPullInDeltaThresh = 0.0002f;
-        static readonly float RotPullInValue = 0.05f;
-        static bool _pullInGateOpen = false;
-        static float _pullInGateTime = 0.1f;
         static float _pullInGateTimer = 0;
+        static bool _pullInGateOpen = false;
 
         static float _stockedMovementAddedPosTarget = 0;
         static float _stockedMovementAddedPosLerp = 0;
-        static readonly float StockMovementAddedPosValue = 0.01f;
 
-        public static void UpdateLerp(float dt)
-        {
-            VerticalPoseUpdate(dt);
-            ChangePoseUpdate(dt);
-            StockedMovUpdate(dt);
-        }
-
-        public static Vector3 GetModifiedHandPosWithPose(Player player)
-        {
-            _currentLerpRate = _lerpRate;
-
-            float poseLevel = player.PoseLevel;
-            _poseVerticalPoseOffsetTarget = (1f - poseLevel) * poseVerticalPoseOffsetModifier;
-            _poseProjectedPoseOffsetTarget = (1f - poseLevel) * poseProjectedPoseOffsetModifier;
-
-            if (player.ProceduralWeaponAnimation.IsAiming)
-            {
-                _poseVerticalPoseOffsetTarget = 0;
-                _poseProjectedPoseOffsetTarget = 0;
-
-                _currentLerpRate = _lerpRate * 2f;
-            }
-
-            return new Vector3(0, _poseVerticalPoseOffsetLerp, _poseProjectedPoseOffsetLerp);
-        }
-
-        public static Vector3 GetModifiedHandPosWithPoseChange(Player player)
-        {
-            Vector3 addedVert = poseShiftVector;
-            float poseLevelThisFrame = player.PoseLevel;
-            if (poseLevelThisFrame != _poseLevelLastFrame)
-            {
-                _vertDiff = poseLevelThisFrame - _poseLevelLastFrame;
-                if (!_isChangingPose)
-                {
-                    _isChangingPose = true;
-                }
-            }
-
-            if (!player.ProceduralWeaponAnimation.IsAiming)
-            {
-                addedVert.y *= 3f;
-            }
-            
-            _poseLevelLastFrame = poseLevelThisFrame;
-            return addedVert;
-        }
-
-        static void ChangePoseUpdate(float dt)
-        {
-            if (_isChangingPose)
-            {
-                float vertDiffAbs = Mathf.Abs(_vertDiff);
-                float speedMod = 1f + (1f - vertDiffAbs);
-                float directionSpeedMod = _vertDiff < 0 ? 1.1f : 0.75f;
-                _changingPoseCycle += dt * speedMod * directionSpeedMod;
-                if (_changingPoseCycle >= 1f)
-                {
-                    _isChangingPose = false;
-                    _changingPoseCycle = 0;
-                    return;
-                }
-                float directionAttenuationMod = _vertDiff < 0 ? 1f : 0.5f;
-                float addedVert = PrimeMover.Instance.PoseChangeCurve.Evaluate(_changingPoseCycle) * vertDiffAbs * _changePoseModifier * directionAttenuationMod;
-                poseShiftVector = new Vector3(0, addedVert, 0);
-            }
-            else
-            {
-                poseShiftVector = Vector3.zero;
-            }
-        }
-
-        public static Vector3 GetModifiedHandPosForRotSpeed(Player player)
+        public static Vector3 GetModifiedHandPosZMovement(Player player)
         {
             float dt = player.DeltaTime;
             _pullInGateTimer += dt;
             if(!player.ProceduralWeaponAnimation.IsAiming)
             {
-                if (_pullInGateTimer > _pullInGateTime)
+                if (_pullInGateTimer > _PullInGateTime)
                 {
                     _pullInGateOpen = true;
                 }
                 float rotDelta = WeaponsHandlingController.RotationDelta;
-                if (rotDelta > RotPullInDeltaThresh && _pullInGateOpen)
+                if (rotDelta > _RotPullInDeltaThresh && _pullInGateOpen)
                 {
                     _pullInGateOpen = false;
                     _pullInGateTimer = 0;
-                    _rotPullInTarget = RotPullInValue;
+                    _rotPullInTarget = _RotPullInValue;
                 }
-                else if (rotDelta <= RotPullInDeltaThresh && _pullInGateOpen)
+                else if (rotDelta <= _RotPullInDeltaThresh && _pullInGateOpen)
                 {
                     _pullInGateOpen = false;
                     _pullInGateTimer = 0;
@@ -144,39 +56,29 @@ namespace TarkovIRL
             {
                 if (WeaponsHandlingController.IsPlayerMovement)
                 {
-                    _rotPullInTarget = RotPullInValue;
+                    _rotPullInTarget = _RotPullInValue;
                 }
                 else
                 {
                     _rotPullInTarget = 0;
                 }
             }
-            _rotPullInLerp = Mathf.Lerp(_rotPullInLerp, _rotPullInTarget, dt * _lerpRate * 0.6f);
+            _rotPullInLerp = Mathf.Lerp(_rotPullInLerp, _rotPullInTarget, dt * _LerpRate * 0.6f);
             return new Vector3(0, 0, -_rotPullInLerp);
         }
 
-        static void VerticalPoseUpdate(float dt)
+        public static Vector3 GetModifiedHandPosForUnstockedMovement(Player player)
         {
-            _poseVerticalPoseOffsetLerp = Mathf.Lerp(_poseVerticalPoseOffsetLerp, _poseVerticalPoseOffsetTarget, dt * _currentLerpRate);
-            _poseProjectedPoseOffsetLerp = Mathf.Lerp(_poseProjectedPoseOffsetLerp, _poseProjectedPoseOffsetTarget, dt * _currentLerpRate);
-        }
-
-        static void StockedMovUpdate(float dt)
-        {
+            float dt = player.DeltaTime;
             if (WeaponsHandlingController.IsStocked && WeaponsHandlingController.IsPlayerMovement)
             {
-                _stockedMovementAddedPosTarget = StockMovementAddedPosValue;
+                _stockedMovementAddedPosTarget = _StockMovementAddedPosValue;
             }
             else
             {
                 _stockedMovementAddedPosTarget = 0;
             }
-
-            _stockedMovementAddedPosLerp = Mathf.Lerp(_stockedMovementAddedPosLerp, _stockedMovementAddedPosTarget, dt * _lerpRate * .4f);
-        }
-
-        public static Vector3 GetModifiedHandPosForUnstockedMovement()
-        {
+            _stockedMovementAddedPosLerp = Mathf.Lerp(_stockedMovementAddedPosLerp, _stockedMovementAddedPosTarget, dt * _LerpRate * 0.4f);
             return new Vector3(0, -_stockedMovementAddedPosLerp, 0);
         }
     }
