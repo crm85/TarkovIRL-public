717db1f28b37afafb8e02fd580d9703e6ffbad35
colin.r.muller@gmail.com <colin.r.muller@gmail.com>
2024-10-10 08:31:03 +0200

    Some new values

--- NewSwayController.cs
+++ NewSwayController.cs
@@ -17,31 +17,32 @@ namespace TarkovIRL
             float isStockedMulti = WeaponController.IsStocked ? -1 : 1f;
             float isStockedAds = WeaponController.IsStocked && PlayerMotionController.IsAiming ? -1 : 1f;
             float isAds = PlayerMotionController.IsAiming && WeaponController.IsStocked ? 0 : 1f;
+            float wpnMulti = WeaponController.GetWeaponMulti(true);
+            float efficiencyMulti = EfficiencyController.EfficiencyModifierInverse;
+            float lerpDTMulti = wpnMulti * efficiencyMulti;
 
             // pos
-            float lerpPosDTMulti = WeaponController.GetWeaponMulti(true);
             float lerpPosTarget = delta * isStockedMulti * isAds;
-            _lerpPos = Mathf.Lerp(_lerpPos, lerpPosTarget, deltaTime * lerpPosDTMulti * PrimeMover.NewSwayPositionDTMulti.Value);
+            _lerpPos = Mathf.Lerp(_lerpPos, lerpPosTarget, deltaTime * lerpDTMulti * PrimeMover.NewSwayPositionDTMulti.Value);
 
             // rot
-            float lerpRotDTMulti = WeaponController.GetWeaponMulti(true);
             float lerpRotTarget = delta * isStockedAds;
-            _lerpRot = Mathf.Lerp(_lerpRot, lerpRotTarget, deltaTime * lerpRotDTMulti * PrimeMover.NewSwayRotationDTMulti.Value);
+            _lerpRot = Mathf.Lerp(_lerpRot, lerpRotTarget, deltaTime * lerpDTMulti * PrimeMover.NewSwayRotationDTMulti.Value);
 
             // debug
-            UtilsTIRL.Log($" deltarot {delta}");
+            UtilsTIRL.Log($" lerpDTMulti {lerpDTMulti}, efficiencyMulti {efficiencyMulti}, wpnMulti {wpnMulti}");
         }
         public static Vector3 GetNewSwayPosition(Player player)
         {
             Vector3 addedSway = Vector3.zero;
-            addedSway.x = _lerpPos * PrimeMover.NewSwayPositionMulti.Value * EfficiencyController.EfficiencyModifier;
+            addedSway.x = _lerpPos * PrimeMover.NewSwayPositionMulti.Value * WeaponController.GetWeaponMulti(false);
             return addedSway;
         }
 
         public static Quaternion GetNewSwayRotation(Player player)
         {
             Quaternion addedRotation = Quaternion.identity;
-            addedRotation.z = _lerpRot * PrimeMover.NewSwayRotationMulti.Value * EfficiencyController.EfficiencyModifier;
+            addedRotation.z = _lerpRot * PrimeMover.NewSwayRotationMulti.Value * WeaponController.GetWeaponMulti(false);
             return addedRotation;
         }
     }
